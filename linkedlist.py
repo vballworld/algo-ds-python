cc14986659e18c3310b8c0987b11928b91204c52
Joe Pak <jpak@irobot.com>
2017-09-20 10:08:54 -0400

    add linkedlist.py

---
+++ linkedlist.py
@@ -0,0 +1,51 @@
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self, node=None):
+        self.head = node
+
+
+    def add_to_head(self, node):
+        if self.head == None:
+            self.head = node
+        else:
+            node.next = self.head
+            self.head = node
+
+
+    def print(self):
+        node = self.head
+
+        if node == None:
+            print("list is empty.")
+
+        while node != None:
+            print("{0}->".format(node.val), end="")
+            node = node.next
+        print("None")
+
+
+    def is_empty(self):
+        if self.head == None:
+            return True
+        else:
+            return False
+
+
+n1 = Node(3)
+n2 = Node(5)
+n3 = Node(9)
+
+l1 = LinkedList()
+
+l2 = LinkedList(n1)
+l2.add_to_head(n2)
+l2.add_to_head(n3)
+l2.print()
+
+print(l2.is_empty())
+print(l1.is_empty())
